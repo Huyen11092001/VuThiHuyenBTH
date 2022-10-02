3f51bc625992736fc755e540805fd3cc6af17a8f
Huyen11092001 <88873971+Huyen11092001@users.noreply.github.com>
2022-10-02 17:04:01 +0700

    VTH_2809

---
+++ Controllers/StringProcessController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using VuThiHuyenBTH.Models.Process;
+
+namespace VuThiHuyenBTH.Controllers
+{
+    public class StringProcessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        StringProcess Str = new StringProcess();
+       
+        [HttpPost]
+        // Xoa bo ky tu trang
+        public IActionResult Index(string strInput)
+        {
+            string TB = Str.RemoveRemainingWhiteSpace(strInput);
+            ViewBag.Thongbao = TB;
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Chuyen chuoi sang in hoa
+        [HttpPost]
+        public IActionResult Create(string strInput)
+        {
+            string abc = Str.LowerToUpper(strInput);
+            ViewBag.Hienthi = abc;
+            return View();
+        }
+        public IActionResult uppertolower()
+        {
+            return View();
+        }
+        // Chuyen chuoi sang in thuong
+        [HttpPost]
+        public IActionResult uppertolower(string strInput)
+        {
+            string up = Str.UpperToLower(strInput);
+            ViewBag.Inthuong = up;
+            return View();
+        }
+        public IActionResult capitaliOne()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        // Viet hoa chu cai dau 
+        public IActionResult capitaliOne(string strInput)
+        {
+            string capitaone = Str.CapitalizeOneFirstCharacter(strInput);
+            ViewBag.Hoa1 = capitaone;
+            return View();
+        }
+        public IActionResult capitaliFirst()
+        {
+            return View();
+        }
+        [HttpPost]
+        // Viet hoa chu cai dau tien cua cac tu trong chuoi
+        public IActionResult capitaliFirst(string strInput)
+        {
+            string capitaFirst = Str.CapitalizeFirstCharacter(strInput);
+            ViewBag.HoaFirst = capitaFirst;
+            return View();
+        }
+        public IActionResult Remove()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Remove(string strInput)
+        {
+            string remove = Str.RemoveVietnameseAccents(strInput);
+            ViewBag.Remo = remove;
+            return View();
+        }
+    }
+}
+
